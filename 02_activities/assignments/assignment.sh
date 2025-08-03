497156a8bd2c011a12e954316d5c122c3f4791c4
Ruchira Malhotra <ruchira.toronto@gmail.com>
2025-08-03 18:31:15 -0400

    last question answered

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -48,6 +48,8 @@ rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+touch ./data/inventory.txt
+ls -R ./data/processed > ./data/inventory.txt
 
 
 ###########################################
